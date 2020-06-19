e8f94d10cd4462ccc42029ad87e83e383be23e19
Seiya Kawamot <kid960805@gmail.com>
2020-06-20 06:48:30 +0900

    firebase fixed

---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase/app';
+import 'firebase/app'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC8PDnFaY6SA_Yg7926JjL9OZqKqoz1u0g",
+    authDomain: "myportfolio-f8227.firebaseapp.com",
+    databaseURL: "https://myportfolio-f8227.firebaseio.com",
+    projectId: "myportfolio-f8227",
+    storageBucket: "myportfolio-f8227.appspot.com",
+    messagingSenderId: "777717311069",
+    appId: "1:777717311069:web:dfcd7273bc7895a790cfb4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- src/views/Work.vue
+++ src/views/Work.vue
@@ -6,15 +6,17 @@
         <v-layout wrap>
             <v-flex>
                 <img src="../assets/marine.png" alt="file" style="width:100%;">
-                <a>
-                    <h2 href="https://stormy-hollows-51545.herokuapp.com/">Marine Biology Book</h2>
+                <a href="https://stormy-hollows-51545.herokuapp.com/"> 
+                    <h2>Marine Biology Book</h2>
                 </a>
                 <p></p>
                 <h3>Laravel/Vue.js/PostgreSQL/Heroku</h3>
                 <p></p>
                 <h4>このアプリは海洋生物の図鑑アプリです。海にはまだ未確認の生物がたくさんいるので、珍しい生物の写真を共有できたらと思い作りました。私は東京海洋大学に通っているので大学でも共有できたらと考えています。技術としてはフロントにVue.js、バックにLaravelを使用しました。SPAとなっています。</h4>
                 <p></p>
-                <v-icon large color="black">{{ mdiGithub }}</v-icon>
+                <a href="https://github.com/seiyawati/vue-laravel-app">
+                    <v-icon large color="black">{{ mdiGithub }}</v-icon>
+                </a>
             </v-flex>
         </v-layout>    
         <p style="height:25px;"></p>
@@ -23,15 +25,17 @@
         <v-layout wrap>
             <v-flex>
                 <img src="../assets/blog.png" alt="" style="width:100%;">
-                <a>
-                    <h2 href="https://seiyablog.herokuapp.com/">Seiya's Tech Blog</h2>
+                <a href="https://seiyablog.herokuapp.com/"> 
+                    <h2>Seiya's Tech Blog</h2>
                 </a>
                 <p></p>
                 <h3>Laravel/Bootstrap/MySQL/Heroku</h3>
                 <p></p>
                 <h4>このブログは自身の技術ブログとして、日々の学習での気付きをメモしています。プログラミングを初めて最初の成果物がこちらになります。なおこのブログはLaravelを用いて一から実装して、デザインにはBootstrapを使用しています。</h4>
                 <p></p>
-                <v-icon large color="black">{{ mdiGithub }}</v-icon>
+                <a href="https://github.com/seiyawati/laravel-my-tech-blog">
+                    <v-icon large color="black">{{ mdiGithub }}</v-icon>
+                </a>
             </v-flex>
         </v-layout>
         <p style="height:25px;"></p>
@@ -46,7 +50,9 @@
                 <p></p>
                 <h4>インターンや就職活動などで自分のことを紹介するために作ったポートフォリオサイトになります。デザインにはUIコンポーネントのVuetifyを使いアニメーションにはTyper.jsなどを使って、Firebaseでホスティングしています。</h4>
                 <p></p>
-                <v-icon large color="black">{{ mdiGithub }}</v-icon>
+                <a href="https://github.com/seiyawati/vue-firebase-portfolio">   
+                    <v-icon large color="black">{{ mdiGithub }}</v-icon>
+                </a>
             </v-flex>
         </v-layout>    
         <p style="height:25px;"></p>
@@ -63,7 +69,9 @@
                 <p></p>
                 <h4>GraphQLについて学習する時、Gatsby.jsの勉強をしたので作成しました。しかし、私はVue.jsをメインにやっていてReact.jsに関してはあまりやっていないので、今はVue.jsで作ったポートフォリオサイトを運用しています。</h4>
                 <p></p>
-                <v-icon large color="black">{{ mdiGithub }}</v-icon>
+                <a href="https://github.com/seiyawati/my-portfolio-gatsby.js">
+                    <v-icon large color="black">{{ mdiGithub }}</v-icon>
+                </a>
             </v-flex>
         </v-layout>
         <p style="height:25px;"></p>
@@ -74,20 +82,11 @@
 
 <script>
 import { mdiGithub } from '@mdi/js';
-import firebase from 'firebase';
 
 export default {
     data: () => ({
         mdiGithub,
-        works: []
-    }),
-    created: () => {
-        firebase.firestore().collection('works').get().then(snapshot => {
-            snapshot.forEach(doc => {
-                this.works.push(doc.data())
-            })
-        })
-    }
+    })
 }
 </script>
 

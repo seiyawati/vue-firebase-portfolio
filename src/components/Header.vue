725eba1c098ca12bd1b16b8d1a135ef4bbcdb1ee
Seiya Kawamot <kid960805@gmail.com>
2020-06-30 23:47:07 +0900

    コメント削除

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,23 +6,6 @@
             <router-link to="Work" class="link">Work</router-link>
             <router-link to="/Contact" class="link">Contact</router-link>
         </div>
-      <!-- <v-app-bar color="#34495e" app dark>
-          <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn text>
-            <router-link to="/" class="link">Home</router-link>
-        </v-btn>
-        <v-btn text>
-            <router-link to="/about" class="link">About</router-link>
-        </v-btn>
-        <v-btn text>
-            <router-link to="Work" class="link">Work</router-link>
-        </v-btn>
-        <v-btn text>
-            <router-link to="/Contact" class="link">Contact</router-link>
-        </v-btn>
-      </v-toolbar-items>
-      </v-app-bar> -->
     </v-container>
 </template>
 
